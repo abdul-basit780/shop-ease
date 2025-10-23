f1e7a751b10b2cd20f0e9ed0fa6100643285e21a
ariful202291976 <marifuli@mun.ca>
2025-10-22 23:47:42 -0230

    url bug fixed login page

--- src/lib/api-client.js
+++ src/lib/api-client.js
@@ -4,7 +4,7 @@ import Cookies from "js-cookie";
 class ApiClient {
   constructor() {
     this.client = axios.create({
-      baseURL: process.env.NEXT_PUBLIC_API_URL || '/api',
+      baseURL: process.env.NEXT_PUBLIC_URL,
       headers: {
         "Content-Type": "application/json",
       },
